03934cd38e889751413073905412a882521db98f
Jiri Sedlacek <jiri.sedlacek@altworx.com>
2019-08-27 16:14:53 +0200

    Bump version to 2.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KafkaGenStage.MixProject do
   def project do
     [
       app: :kafka_gen_stage,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.7",
       package: package(),
       description: "Klarna's Brod kafka client wrapped into GenStage.",
