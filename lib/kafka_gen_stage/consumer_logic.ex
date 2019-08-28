1b1a4c5d923524c7a7747d942286efdeb6e31e1e
Tomas Janousek <tomas.janousek@altworx.com>
2019-08-28 14:21:30 +0200

    Simplify KafkaGenStage.ConsumerLogic.dequeue
    Move to_ack handling out of it so that all it does is the actual
    dequeueing, and drop the now unnecessary type definitions.
    
    (Also, inline lower_demand as it's no longer used multiple times and its
    definition is about as long as the call.)

--- lib/kafka_gen_stage/consumer_logic.ex
+++ lib/kafka_gen_stage/consumer_logic.ex
@@ -14,16 +14,6 @@ defmodule KafkaGenStage.ConsumerLogic do
   @typedoc "Last offset (inclusive) to be emmited by GenStage."
   @type end_offset :: non_neg_integer() | :infinity
 
-  @typedoc "Internal type for working with demand and event buffers."
-  @type dispatch ::
-          {msgs_to_send :: [msg_tuple()], ack_to_brod_consumer :: ack(),
-           buffered_msgs :: :queue.queue()}
-
-  @typedoc "API type for working with demand and event buffers."
-  @type return_dispatch ::
-          {msgs_to_send :: [msg_tuple()], ack_to_brod_consumer :: ack(),
-           buffered_demand :: non_neg_integer(), buffered_msgs :: :queue.queue()}
-
   @doc """
   Handles caching of both, demand and events, as recomended in *Buffering Demand* in gen_stage docs.
   Thus called from both, `KafkaGenStage.Consumer.handle_demand/2` and
@@ -34,16 +24,27 @@ defmodule KafkaGenStage.ConsumerLogic do
           buffered_demand :: non_neg_integer(),
           bulk_transformer :: ([msg_tuple()] -> [msg_tuple()]),
           is_end_of_stream :: boolean()
-        ) ::
-          return_dispatch()
+        ) :: {
+          msgs_to_send :: [msg_tuple()],
+          ack_to_brod_consumer :: ack(),
+          buffered_demand :: non_neg_integer(),
+          buffered_msgs :: :queue.queue()
+        }
   def prepare_dispatch(queue, 0, _bulk_transformer, _is_end_of_stream) do
     {[], :no_ack, 0, queue}
   end
 
   def prepare_dispatch(queue, demand, bulk_transformer, is_end_of_stream) do
-    {to_send, to_ack, queue} = dequeue(queue, demand, [], :no_ack)
+    {to_send, queue} = dequeue(queue, demand)
+
+    to_ack =
+      case to_send do
+        [] -> :no_ack
+        [{offset, _, _, _} | _] -> offset
+      end
+
     to_send = to_send |> Enum.reverse() |> transform_bulk(bulk_transformer, is_end_of_stream)
-    demand = lower_demand(demand, to_send)
+    demand = max(0, demand - length(to_send))
     {to_send, to_ack, demand, queue}
   end
 
@@ -55,35 +56,17 @@ defmodule KafkaGenStage.ConsumerLogic do
     bulk_transformer.(to_send, is_end_of_stream)
   end
 
-  @spec dequeue(
-          buffered_msgs :: :queue.queue(),
-          buffered_demand :: non_neg_integer(),
-          msgs_to_send :: [term()],
-          to_ack :: ack()
-        ) :: dispatch()
-  defp dequeue(queue, 0, to_send, to_ack) do
-    {to_send, to_ack, queue}
+  defp dequeue(queue, demand, dequeued \\ [])
+
+  defp dequeue(queue, 0, dequeued) do
+    {dequeued, queue}
   end
 
-  defp dequeue(queue, demand, to_send, to_ack) do
+  defp dequeue(queue, demand, dequeued) do
     case :queue.out(queue) do
-      {{:value, {offset, _, _, _} = msg}, queue} ->
-        dequeue(
-          queue,
-          demand - 1,
-          [msg | to_send],
-          offset
-        )
-
-      {:empty, queue} ->
-        {to_send, to_ack, queue}
+      {{:value, msg}, queue} -> dequeue(queue, demand - 1, [msg | dequeued])
+      {:empty, queue} -> {dequeued, queue}
     end
-  end
-
-  # its ok to send more than demanded events
-  defp lower_demand(demand, to_send) do
-    new_demand = demand - length(to_send)
-    if new_demand < 0, do: 0, else: new_demand
   end
 
   @doc """
